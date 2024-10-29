ff5ba6a5abca86139771e9efe18a962d67748a89
João Picoli <96037352+JoaoPicoli@users.noreply.github.com>
2024-10-29 17:02:12 -0300

    Add files via upload

---
+++ dialogue_data.py
@@ -0,0 +1,235 @@
+#dialogue databank
+
+yke_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+floris_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+morganna_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+calistus_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+bram_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+gerhard_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+aline_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+alaric_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+isolde_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+eleonora_dialogue = {
+    1: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    2: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    3: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ],
+    4: [
+        {"line": "", "value": "x"},
+        {"line": "", "value": "y"},
+        {"line": "", "value": "z"}
+    ]
+}
+
+
+
+
